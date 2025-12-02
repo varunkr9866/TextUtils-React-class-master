c3ca827339a4ae91b398b0903a0c261953fc5e2b
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-02 14:26:49 +0530

    COMPLETED WORKING ON app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import './App.css';
+import About from './components/About.jsx';
+import Navbar from './components/Navbar.jsx';
+import Alert from './components/Alert.jsx';
 
 function App() {
   return (
     <>
-    
+    <About />
+    <Navbar />
+    <Alert />
     </>
   );
 }
